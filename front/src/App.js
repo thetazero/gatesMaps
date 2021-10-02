8592dde755bece1a4f842f6798e09ebb6f3c9cf9
Adam Bournes <s151478@scusd.net>
2021-10-01 22:20:42 -0400

    help me

--- front/src/App.js
+++ front/src/App.js
@@ -5,19 +5,41 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        className="App-link"
+        href="https://www.cmu.edu/finance/property-space/floorplan-room/acad-admin/GHC/index.html"
+        target="_blank"
+        rel="noopener noreferrer"
         >
-          Learn React
+        Gates Map
         </a>
+
+        <p>
+          Starting Location:
+        </p>
+
+        <input type="text" id="myText1" value="Dog type in yo location">
+        </input>
+
+        <p>
+          Destination:
+        </p>
+
+        <input type="text" id="myText2" value="Dog type in yo destination">
+        </input>
+
+        var time = 0:00
+        <p>
+        Estimated time: <time></time>
+        </p>
+
+        <img src={logo} className="App-logo" alt="logo" />
+        
       </header>
+      <body>
+
+      </body>
     </div>
   );
 }
