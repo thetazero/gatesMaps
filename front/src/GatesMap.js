9bbc39e4066fa4a126c23fed54260d5cb87c8e47
thetazero <lesha.seletskiy@gmail.com>
2021-10-02 14:14:50 -0400

    cry

--- front/src/GatesMap.js
+++ front/src/GatesMap.js
@@ -1,28 +1,28 @@
-import { useState } from "react"
+// import { useState } from "react"
 
 
-export default function GatesMap({ camX, camY, path }) {
-  let [img, setImg] = useState('')
-  let [canvas, setCanvas] = useState('')
-  let [ctx, setCtx] = useState('')
-  function magic() {
-    console.log('hello')
-    let c = document.getElementById("canvas")
-    setCanvas(c)
-    let ct = c.getContext('2d')
-    setCtx(ct)
-    let image = new Image()
-    image.onload = () => {
-      ct.drawImage(this, 0, 0)
-    }
-    image.src = 'data:image/svg+xml,' + escape(floor7)
-    ct.drawImage(floor7, 33, 71, 104, 124, 21, 20, 87, 104)
+// export default function GatesMap({ camX, camY, path }) {
+//   let [img, setImg] = useState('')
+//   let [canvas, setCanvas] = useState('')
+//   let [ctx, setCtx] = useState('')
+//   function magic() {
+//     console.log('hello')
+//     let c = document.getElementById("canvas")
+//     setCanvas(c)
+//     let ct = c.getContext('2d')
+//     setCtx(ct)
+//     let image = new Image()
+//     image.onload = () => {
+//       ct.drawImage(this, 0, 0)
+//     }
+//     image.src = 'data:image/svg+xml,' + escape(floor7)
+//     ct.drawImage(floor7, 33, 71, 104, 124, 21, 20, 87, 104)
 
-  }
-  return (
-    <>
-      <button onClick={magic}>magic</button>
-      <canvas id="canvas"></canvas>
-    </>
-  )
-}
+//   }
+//   return (
+//     <>
+//       <button onClick={magic}>magic</button>
+//       <canvas id="canvas"></canvas>
+//     </>
+//   )
+// }
