4ed844bee597d15ded17b1a8d7cfc2fdb06f5092
Yonah Goldberg <yonah.goldberg@icloud.com>
2021-10-02 12:29:33 -0400

    younah is cringee and cringe

--- back/graph.js
+++ back/graph.js
@@ -2,31 +2,41 @@ const Graph = require('node-dijkstra')
 const fs = require('fs')
 
 //const floor1 = require('../data/sigma.json')
-const floor7 = require('../data/floor7.json')
+const floor7 = require('../data/floor7-s.json')
 const floor7Edges = require('../data/floor7-edges.json') 
 const curGraph = require('./graphSave.json')
 let src = Object.keys(curGraph) == 0 ? floor7 : curGraph
 const route = new Graph(src)
-//const descriptionMap = get shit from json
-//route.graph.get('A').set('B', 2)
+
+function hasEdge(a,b){
+  return route.graph.get(a).get(b)!=null
+}
 
 function getRoute(from, to) {
-  let route = route.path(from, to)
-  for (let i = 1; i < route.length - 1; i++) {
-    if (!route[i].startsWith("7")) {
-      route.splice(i, 1)
-    }
-  }
-  return route
+  let path = route.path(from, to)
+  console.log(path)
+  
+  
+  path=path.filter((e,i)=>{
+    //make sure that we only remove this node if it can be removed
+    if(i > 0 && i < path.length - 1 && !hasEdge(path[i-1], path[i+1])) return true 
+    return (i==0 || i==path.length-1 || e[0]!="7")
+  })
+
+
+
+  console.log(path)
+  return path
 }
 module.exports.getRoute = getRoute
 
 function describeRoute(nodes) {
   //@saftey and correctness
   let description = []
-  for (let i = 0; i < nodes.length - 1; i++) {
+  for (let i = 1; i < nodes.length - 2; i++) {
     let edge = nodes[i] + nodes[i + 1]
-    description.push(`Turn towards ${floor7Edges.edge.des}.`)
+    console.log(edge)
+    description.push(`Turn towards ${floor7Edges[edge].des}.`)
   }
   description.push(`End at ${nodes[nodes.length - 1]}.`)
   return description
